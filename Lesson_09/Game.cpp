8a0797a43bb0836c6814a3b5a186849efc7a9d10
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2021-04-04 23:03:13 +0900

    Lesson_09と10を仮で追加、仮のstageモデル追加。

---
+++ Lesson_09/Game.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include "Game.h"
+#include "Player.h"
+#include "BackGround.h"
+#include "GameCamera.h"
+#include "Star.h"
+
+//Hands On 3 SoundEngineとWaveFileBankの機能を使いたいので。
+//ヘッダーファイルをインクルードする。
+
+
+Game::Game()
+{
+	NewGO<BackGround>(0);
+	auto player = NewGO<Player>(0);
+	auto gameCamera = NewGO<GameCamera>(0);
+	gameCamera->player = player;
+
+	Star* star = NewGO<Star>(0);
+
+	//Hands On 4 WaveFileBankを使って、waveファイル(.wav)を読み込む。
+	
+
+	//Hands On 5 BGMを再生してみよう。
+
+}
+
+Game::~Game()
+{
+
+}
+
+void Game::Update()
+{
+
+}
